d75a34f8a697c2e7e7ff065a830212f0565cdffb
Philipp Arndt <2f.mail@gmx.de>
2019-03-16 14:28:41 +0100

    fixed path

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mvn clean install assembly:single
 if [ $? -eq 0 ]; then
-    scp ./de.rnd7.ws/ws.jar 192.168.3.104:/home/pi/ws.jar
+    scp ./de.rnd7.ws/target/ws.jar 192.168.3.104:/home/pi/ws.jar
     echo "deploy finished"
 else
     echo FAIL
